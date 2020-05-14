b4a3b3ca14211417c420dfc691bc1203e0fd8303
Radi Becker <radibek@gmail.com>
2020-05-14 12:50:24 +0300

    first commit

---
+++ TATestAPI/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TATestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public UsersController()
+        {
+            _connectionString = @"Data Source=M2039955-W7\SQL2014C;Initial Catalog=RADI_DB;Integrated Security=False;User ID=RADI_DB;Password=RADI_DB";
+        }
+
+        
+        [HttpGet]
+        public List<User> Get()
+        {
+            List<User> users = readFromDB();
+
+            return users;
+        }
+
+
+        private List<User> readFromDB()
+        {
+            List<User> list = new List<User>();
+
+            try
+            {
+
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    String query = "SELECT * FROM Users";
+
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader rdr = cmd.ExecuteReader())
+                        {
+                            while (rdr.Read())
+                            {
+                                User user = new User();
+                                user.Id = rdr.GetInt32(0);
+                                user.Name = rdr.GetString(1);
+                                list.Add(user);
+                                Console.WriteLine("{0} {1}", user.Id, user.Name);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                User user = new User();
+                user.Id = 1;
+                user.Name = "Test";
+
+                list.Add(user);
+                return list;
+            }
+           
+
+            return list;
+        }
+    }
+
+
+    public class User
+    {
+        private int id;
+        private string name;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ TATestAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+
+namespace TATestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+     
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+       
+
+
+           //// string sql = "SELECT * FROM cars";
+           // using var cmd = new SqlCommand(sql, con);
+
+           //// using SqlDataReader rdr = cmd.ExecuteReader();
+
+           // while (rdr.Read())
+           // {
+           //     Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1),
+           //             rdr.GetInt32(2));
+           // }
+
+        }
+
+        //private void insertToDB()
+        //{
+        //    using (SqlConnection connection = new SqlConnection(_connectionString))
+        //    {
+        //        String query = "INSERT INTO dbo.SMS_PW (id,username,password,email) VALUES (@id,@username,@password, @email)";
+
+        //        using (SqlCommand command = new SqlCommand(query, connection))
+        //        {
+        //            command.Parameters.AddWithValue("@id", "abc");
+        //            command.Parameters.AddWithValue("@username", "abc");
+        //            command.Parameters.AddWithValue("@password", "abc");
+        //            command.Parameters.AddWithValue("@email", "abc");
+
+        //            connection.Open();
+        //            int result = command.ExecuteNonQuery();
+
+        //            // Check Error
+        //            if (result < 0)
+        //                Console.WriteLine("Error inserting data into Database!");
+        //        }
+        //    }
+        //}
+
+
+
+    }
